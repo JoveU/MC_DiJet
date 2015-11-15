3b16be5aa71ceeec08666ad2eb5b0d0507e0ffe7
Vladi <Vladi@Skokov.net>
2015-11-15 18:26:37 -0500

    Comments and code cleaning

--- interp2d/CTestTestfile.cmake
+++ interp2d/CTestTestfile.cmake
@@ -1,7 +1,7 @@
 # CMake generated Testfile for 
-# Source directory: /home/vskokov/MC_DiJet/interp2d
-# Build directory: /home/vskokov/MC_DiJet/interp2d
+# Source directory: /tmp/MC_DiJet/interp2d
+# Build directory: /tmp/MC_DiJet/interp2d
 # 
-# This file includes the relevent testing commands required for 
+# This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(all "interp2dtest")
+add_test(all "interp2dtest")
